4f2e50d76c3882615c3a713ea29a0a3c8e8786fd
jbrockdev <jbrockwork33@osgsmail.icu>
2021-09-22 22:46:18 -0700

    added external weather source

--- server.js
+++ server.js
@@ -2,19 +2,21 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT;
-const weatherData = require("./data/weather.json");
+const PORT = process.env.PORT || 3133;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 const cors = require("cors");
+const axios = require("axios");
 app.use(cors());
 
 const forecastDomain = require("./domain/Forecast.js");
 const { response } = require("express");
 
-app.get("/", (request, response) => {
-  response.status(200).send("Test");
-});
+app.get("/", getHome);
 
-app.get("/weather", (request, response) => {
+app.get("/weather", getWeather);
+
+function getWeather(request, response) {
   let cityQuery = request.query.searchQuery;
   if (cityQuery) {
     cityQuery = cityQuery.toLowerCase();
@@ -33,14 +35,14 @@ app.get("/weather", (request, response) => {
     );
   }
   try {
-    const cityData = weatherData.find(
-      (city) =>
-        city.city_name.toLowerCase() === cityQuery &&
-        city.lon === longitude &&
-        city.lat === latitude
-    );
-    let dailyForecast = getForecast(cityData);
-    response.status(200).send(dailyForecast);
+    let apiResponse = axios
+      .get(
+        `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${latitude}&lon=${longitude}`
+      )
+      .then((weatherResponse) => {
+        let dailyForecast = getForecast(weatherResponse.data);
+        response.status(200).send(dailyForecast);
+      });
   } catch (error) {
     handleError(
       500,
@@ -48,7 +50,11 @@ app.get("/weather", (request, response) => {
       response
     );
   }
-});
+}
+
+function getHome(request, response) {
+  response.status(200).send("Proof of life: 33");
+}
 
 function handleError(errorCode, errorMessage, response) {
   response.status(errorCode).send(errorMessage);
@@ -56,11 +62,15 @@ function handleError(errorCode, errorMessage, response) {
 
 function getForecast(cityData) {
   let forecastArray = [];
+  if (!cityData.status === 200) {
+    return forecastArray;
+  }
+  console.log(cityData.data.data);
   cityData.data.forEach((forecast) => {
     let description = "Low of " + forecast.app_min_temp;
     description += ", high of " + forecast.app_max_temp;
     description += " with " + forecast.weather.description.toLowerCase();
-    let date = forecast.valid_date;
+    let date = forecast.datetime;
     forecastArray.push(new forecastDomain.Forecast(date, description));
   });
   return forecastArray;
